ff816b80c8523c446468a95dc8758d79c467e38e
Sophie <sallysophie@hotmail.de>
2024-11-23 09:52:00 +0100

    Shuffle answers, add quiz completed screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizComplete from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-  const curQuestion = QUESTIONS[activeQuestionIndex];
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = (answer) => {
     setUserAnswers((prevAnswers) => [answer, ...prevAnswers]);
   };
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizComplete} alt="trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffeledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffeledAnswers.sort(() => Math.random - 0.5);
+
   return (
     <>
       <div id="quiz">
         <div id="question">
-          <h2>{curQuestion.text}</h2>
+          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
           <ul id="answers">
-            {curQuestion.answers.map((answer) => (
+            {shuffeledAnswers.map((answer) => (
               <li key={answer} className="answer">
                 <button onClick={() => handleSelectAnswer(answer)}>
                   {answer}
